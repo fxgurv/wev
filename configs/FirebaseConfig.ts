1126c831f94ecd920b96da31e0aae63e3d21bc50
Strange <168190107+fxgurv@users.noreply.github.com>
2024-12-24 01:23:41 +0500

    Update FirebaseConfig.ts

--- configs/FirebaseConfig.ts
+++ configs/FirebaseConfig.ts
@@ -1,23 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "ai-shorts-e4688.firebaseapp.com",
-  projectId: "ai-shorts-e4688",
-  storageBucket: "ai-shorts-e4688.appspot.com",
-  messagingSenderId: "1096178135327",
-  appId: "1:1096178135327:web:ca6b5c0953a2baf766002a",
-  measurementId: "G-Q4NZHRB5WW",
+  apiKey: "AIzaSyCEMqMuUDKZUpETxyLMXJ52Xs0Dw3vN5aI",
+  authDomain: "shorts-6e2bf.firebaseapp.com",
+  projectId: "shorts-6e2bf",
+  storageBucket: "shorts-6e2bf.firebasestorage.app",
+  messagingSenderId: "266895143110",
+  appId: "1:266895143110:web:cd70698a39cebf6a3f27c7",
+  measurementId: "G-LYW70RLW16"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
